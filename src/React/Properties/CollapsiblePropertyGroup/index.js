a9e13553b3303d2f413ce795af1f800d8274b618
Scott Wittenburg <scott.wittenburg@kitware.com>
2017-06-15 21:42:15 -0600

    Keep track of group collapse state externally
    Allowing to make this property editor a pure stateless function

--- src/React/Properties/CollapsiblePropertyGroup/index.js
+++ src/React/Properties/CollapsiblePropertyGroup/index.js
@@ -2,36 +2,28 @@ import React     from 'react';
 import style     from 'PVWStyle/ReactProperties/CollapsiblePropertyGroup.mcss';
 import factory   from '../../Properties/PropertyFactory';
 
+const CollapsiblePropertyGroup = (props) => {
+  const isCollapsed = !props.prop.data.value[0];
+  const id = props.prop.data.id;
 
-export default React.createClass({
+  return (
+    <div className={props.show(props.viewData) ? style.container : style.hidden}>
+      <div className={style.toolbar} onClick={() => props.onGroupCollapseChange(id, !isCollapsed)}>
+        <i className={isCollapsed ? style.collapsedIcon : style.expandedIcon} />
+        <span className={style.title}>{props.prop.ui.label}</span>
+      </div>
+      <div className={isCollapsed ? style.hidden : style.content}>
+        {props.prop.children.map(p => factory(p, props.viewData, props.onChange))}
+      </div>
+    </div>);
+};
 
-  displayName: 'CollapsiblePropertyGroup',
+CollapsiblePropertyGroup.propTypes = {
+  show: React.PropTypes.func,
+  prop: React.PropTypes.object,
+  viewData: React.PropTypes.object,
+  onChange: React.PropTypes.func,
+  onGroupCollapseChange: React.PropTypes.func,
+};
 
-  propTypes: {
-    show: React.PropTypes.func,
-    prop: React.PropTypes.object,
-    viewData: React.PropTypes.object,
-    onChange: React.PropTypes.func,
-    collapsed: React.PropTypes.bool,
-    onGroupCollapseChange: React.PropTypes.func,
-  },
-
-  getDefaultProps() {
-    return {
-      collapsed: true,
-    };
-  },
-
-  render() {
-    return (
-      <div className={this.props.show(this.props.viewData) ? style.container : style.hidden}>
-        <div className={style.toolbar} onClick={() => this.props.onGroupCollapseChange(!this.props.collapsed)}>
-          <i className={this.state.collapsed ? style.collapsedIcon : style.expandedIcon} />
-          <span className={style.title}>{this.props.prop.ui.label}</span>
-        </div>
-        <div className={this.state.collapsed ? style.hidden : style.content}>
-          {this.props.prop.children.map(p => factory(p, this.props.viewData, this.props.onChange))}
-        </div>
-      </div>);
-  },
-});
+export default CollapsiblePropertyGroup;
